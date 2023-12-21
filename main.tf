6184553b7257b8938a02492b2b0ee737f009158f
KhacDuy <duypk@duy-mbp-14.local>
2023-12-21 23:04:51 +0700

    init repo

---
+++ main.tf
@@ -0,0 +1,162 @@
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "AWSKEY.pem"
+}
+
+resource "aws_key_pair" "generated" {
+  key_name   = "AWSKEY"
+  public_key = tls_private_key.generated.public_key_openssh
+}
+
+
+
+data "aws_availability_zones" "az" {
+
+}
+
+# Terraform Data Block - Lookup Ubuntu 22.04
+data "aws_ami" "ubuntu_22_04" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  vpc_id     = aws_vpc.vpc.id
+  count      = 1
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
+  # cidr_block = var.public_subnets[count.index]
+  availability_zone       = data.aws_availability_zones.az.names[count.index]
+  map_public_ip_on_launch = false
+  tags = {
+    "Name" = "Public subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  count          = 1
+  route_table_id = element(aws_route_table.public_route_table.*.id, count.index)
+  subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
+}
+# Security Groups
+resource "aws_security_group" "ingress-ssh" {
+  name   = "allow-all-ssh"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+  // Terraform removes the default rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create Security Group - Web Traffic
+resource "aws_security_group" "vpc-web" {
+  name        = "vpc-web-${terraform.workspace}"
+  vpc_id      = aws_vpc.vpc.id
+  description = "Web Traffic"
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "vpc-ping" {
+  name        = "vpc-ping"
+  vpc_id      = aws_vpc.vpc.id
+  description = "ICMP for Ping Access"
+  ingress {
+    description = "Allow ICMP Traffic"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outboun"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ubuntu_server" {
+  ami           = data.aws_ami.ubuntu_22_04.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets[0].id
+  security_groups = [aws_security_group.vpc-ping.id,
+  aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.generated.key_name
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+  }
+  tags = {
+    Name = "Ubuntu EC2 Server"
+  }
+  provisioner "local-exec" {
+    command = "chmod 600 ${local_file.private_key_pem.filename}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo rm -rf /tmp",
+      "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
+      "sudo sh /tmp/assets/setup-web.sh",
+    ]
+  }
+}
---
+++ terraform.tf
@@ -0,0 +1,41 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-duypk"
+    key = "prod/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+  # backend "http" {
+  #   address        = "http://localhost:8080/terraform_state/my_state"
+  #   lock_address   = "http://localhost:8080/terraform_lock/my_state"
+  #   lock_method    = "PUT"
+  #   unlock_address = "http://localhost:8080/terraform_lock/my_state"
+  #   unlock_method  = "DELETE"
+  # }
+  required_version = ">= 1.4.0"
+  required_providers {
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.5"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
